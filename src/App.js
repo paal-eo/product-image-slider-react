858b1c43a39bca5f1742811a42ffd8f7a7b68933
Pål Stakvik Extraoptical <paal@extraoptical.com>
2022-06-16 12:35:40 +0200

    gallery and ability to set new active image by clicking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+import img0 from "./images/a2005_black_front.webp";
+import img1 from "./images/a2005_black_face.webp";
+import img2 from "./images/a2005_black_side1.webp";
+import img3 from "./images/a2005_black_side2.webp";
+
+
+import leftArr from "./images/navigation/chevron-left.svg";
+import rightArr from "./images/navigation/chevron-right.svg";
+import { useState } from 'react';
+
+
 function App() {
+
+
+
+  const chooseNext = () => {
+
+  }
+
+  const images = [
+    { i: 0, src: img0 },
+    { i: 1, src: img1 },
+    { i: 2, src: img2 },
+    { i: 3, src: img3 },
+  ]
+
+  const [activeImg, setActiveImg] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className='featured' src={images[activeImg].src} alt="" />
+      <div className="slide-wrapper">
+        <img className='arrow' onClick={() => chooseNext(-1)} src={leftArr} alt="" />
+        <div className="slider">
+          {images.map(e => {
+            return <img onClick={() => setActiveImg(e.i)} className={`thumbnail ${e.i === activeImg ? 'active' : ''}`} src={e.src} alt="" />
+          })}
+        </div>
+        <img className='arrow' onClick={() => chooseNext(1)} src={rightArr} alt="" />
+      </div>
     </div>
   );
 }
